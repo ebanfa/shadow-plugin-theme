a2400af745cb68e0b746ce8c57dae5d8ad619e63
Edward Banfa <ebanfa@gmail.com>
2016-02-01 16:31:05 +0300

    Shadow origin

--- page-templates/content-report.php
+++ page-templates/content-report.php
@@ -7,6 +7,6 @@
     $pdf = new FPDF();
     $pdf->AddPage(); 
     $pdf->SetFont('Courier', 'B', 16);
-    $pdf->Cell(40,10, 'Lilian demo');
+    $pdf->Cell(40,10, 'HPMS Demo Report');
     $pdf->Output();
 ?>
